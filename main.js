357ec228aa0987aef9e203fe4a8ad4d61781c605
i09252003 <108205798+i09252003@users.noreply.github.com>
2022-08-07 19:34:31 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let yes = 'Hindi kita gusto, pero "Mahal" na kita(type "person" if you want to know who the creator is.)';
+let no = 'putanginamo(type "person" if you want to know who the creator is.)';
+const person = {
+firstName: 'Ibrahim',
+lastName: 'Fernandez',
+feelings: 'unknown',
+age: '20 going 21',
+height: '5.11 ata',
+LovesYou: 'of course!'
+};
+
+console.log('Gusto mo bang malaman kung may gusto ako sayo? "type: yes or no"');
+alert("Hi babeee!!!");
+alert('fuck you ╭∩╮（︶︿︶）╭∩╮');
+alert('de jok lang, I LOVE YOU!!! <3')
+alert('This website is just for Jewel Requora only, if you are not that subject then this website is not for you')
